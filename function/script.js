c324a23d3bf45f278d98b8d0c038863e648d7188
vnlopes <viniciuslopesdasilva01@gmail.com>
2024-03-24 11:21:21 -0300

    emoji appears on click

--- function/script.js
+++ function/script.js
@@ -1,42 +1,38 @@
+arrImg = [
+  "🤑",
+  "💩",
+  "🤬",
+  "🙊",
+  "🤪",
+  "🤯",
+  "🤑",
+  "💩",
+  "🤬",
+  "🙊",
+  "🤪",
+  "🤯",
+];
+
+let arrRand = arrImg.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+let cont = 12;
 
 const vire = (aqui) => {
+  cont--;
+
   aqui.classList.add("virar");
+  aqui.innerHTML = arrImg[cont];
 };
 
-
-arrImg = ["🤑", "💩", "🤬", "🙊", "🤪", "🤯",];
-
-let arrRand = arrImg.sort(() => (Math.random() > 0.5)? 2 : -1);
-
-
-arrImg2 = ["🤑", "💩", "🤬", "🙊", "🤪", "🤯",];
-
-let arrRand2 = arrImg2.sort(() => (Math.random() > 0.5)? 2 : -1);
-
-
-
-const constructor = () => {
-  document.querySelector(".one1").innerHTML = arrImg[0]
-  document.querySelector(".two2").innerHTML = arrImg[1]
-  document.querySelector(".tree3").innerHTML = arrImg[2]
-  document.querySelector(".four4").innerHTML = arrImg[3]
-  document.querySelector(".five5").innerHTML = arrImg[4]
-  document.querySelector(".six6").innerHTML = arrImg[5]
-  document.querySelector(".seven7").innerHTML = arrImg2[0]
-  document.querySelector(".eigth8").innerHTML = arrImg2[1]
-  document.querySelector(".nine9").innerHTML = arrImg2[2]
-  document.querySelector(".ten10").innerHTML = arrImg2[3]
-  document.querySelector(".eleven11").innerHTML = arrImg2[4]
-  document.querySelector(".twoelve12").innerHTML = arrImg2[5]
-};
-
-constructor()
-
-
-const manter = () => {
-  // if(arrRand.indexOf(2) == arrRand2.indexOf(1)){
-  //   alert('dasd')
-  // }
-}
-
-manter()
+// document.querySelector(".one1").innerHTML
+// document.querySelector(".two2").innerHTML
+// document.querySelector(".tree3").innerHTML
+// document.querySelector(".four4").innerHTML
+// document.querySelector(".five5").innerHTML
+// document.querySelector(".six6").innerHTML
+// document.querySelector(".seven7").innerHTML
+// document.querySelector(".eight8").innerHTML
+// document.querySelector(".nine9").innerHTML
+// document.querySelector(".ten10").innerHTML
+// document.querySelector(".eleven11").innerHTML
+// document.querySelector(".twoelve12").innerHTML
